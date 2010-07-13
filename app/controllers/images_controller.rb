0c3463f1504cd13468b59e909dc44716e4a2f26c
pp <zjinys@gmail.com>
2010-07-13 16:49:24 +0800

    1. add project , author relation to image model 2. add new method

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -9,6 +9,19 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.new(:project => @project, :author => User.current)
+    if request.post?
+      @image.attributes = params[:image]
+      if @image.save
+        attachments = Attachment.attach_files(@image, params[:attachments])
+        render_attachment_warning_if_needed(@image)
+
+        flash[:notice] = l(:notice_successful_create)
+        redirect_to :action => "index", :project_id => params[:project_id]
+      else
+        render :action => "new", :id => params[:id]
+      end
+    end
   end
 
   def show
