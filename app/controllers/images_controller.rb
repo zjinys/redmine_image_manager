c55f25b8875501f2b0bcafd82e52990358f0ddec
pp <zjinys@gmail.com>
2010-07-13 22:47:22 +0800

    1. can upload attachment to a image 2. add chinese translation

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,8 +3,10 @@ class ImagesController < ApplicationController
 
   before_filter :find_project
   
+  helper :attachments
+  include AttachmentsHelper
+  
   def index
-    
     @images = Image.find(:all,:conditions=>['status=1 and project_id=?',@project.id])
   end
 
@@ -13,8 +15,8 @@ class ImagesController < ApplicationController
     if request.post?
       @image.attributes = params[:image]
       if @image.save
-        #attachments = Attachment.attach_files(@image, params[:attachments])
-        #render_attachment_warning_if_needed(@image)
+        attachments = Attachment.attach_files(@image, params[:attachments])
+        render_attachment_warning_if_needed(@image)
 
         flash[:notice] = l(:notice_successful_create)
         redirect_to :action => "index", :project_id => params[:project_id]
@@ -25,6 +27,7 @@ class ImagesController < ApplicationController
   end
 
   def show
+    @image = Image.find_by_id(params[:id])    
   end
 
   private
--- app/models/image.rb
+++ app/models/image.rb
@@ -2,5 +2,5 @@ class Image < ActiveRecord::Base
   belongs_to :project,:class_name => 'Project', :foreign_key => 'project_id'
   belongs_to :author, :class_name => 'User', :foreign_key => 'author_id'
 
-  
+  acts_as_attachable
 end
